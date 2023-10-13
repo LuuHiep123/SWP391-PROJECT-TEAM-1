1ad3fbbef3482e52ebc5553e019b0dce258f74d9
datdua <toimqn09@gmail.com>
2023-10-14 03:00:26 +0700

    demo1_FrontEnd

--- project-src/Font-End/FE-ESMS/js/script.js
+++ project-src/Font-End/FE-ESMS/js/script.js
@@ -11,29 +11,3 @@ showTextLink.addEventListener('click', () => {
         isTextVisible = false;
     }
 });
-
-$(document).ready(function () {
-    // Initialize the first DataTable
-    $('#example').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        select: {
-            style: 'multi'
-        }
-    });
-
-    // Initialize the second DataTable
-
-    $('#example1').DataTable({
-        dom: 'Bfrtip',
-        buttons: [
-            'copy', 'csv', 'excel', 'pdf', 'print'
-        ],
-        select: {
-            style: 'multi'
-        }
-    });
-});
-
