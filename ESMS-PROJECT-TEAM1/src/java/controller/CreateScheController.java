a0923f317f074e1a04690adf216cc37672d36f55
LAPTOP-1GLJOCP7\Admin <quangbui300323@gmail.com>
2023-10-30 17:45:38 +0700

    quang push funtion

---
+++ ESMS-PROJECT-TEAM1/src/java/controller/CreateScheController.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.*;
+import dto.*;
+import dao.UserDAO;
+
+import java.time.LocalTime;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import dto.UserDTO;
+
+/**
+ *
+ * @author Admin
+ */
+public class CreateScheController extends HttpServlet {
+private static final String ERROR = "create.jsp";
+    private static final String SUCCESS = "TestingStaff.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        StudentExamDTO StudentExam = new StudentExamDTO();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try  {
+              StudentExamDAO dao = new StudentExamDAO();
+            boolean checkValidation = true;
+            String SubjectID = request.getParameter("Subject_ID");
+            String StartTime = request.getParameter("StartTime");
+            String EndTime = request.getParameter("EndTime");
+            String DateString = request.getParameter("Date");
+            Date dob = null;
+            dob = dateFormat.parse("DateString");
+            int Room =Integer.parseInt(request.getParameter("Room"));
+            String Campus = request.getParameter("Campus");
+            String Form = request.getParameter("Form");
+            
+            
+            
+ 
+            
+            if(SubjectID.length() < 2 || SubjectID.length() > 10){
+                StudentExam.setSubjectID("SubjectID must be in [2,10]");
+                checkValidation = false;
+            }
+           
+           
+           
+           
+           
+                   
+           if(checkValidation){
+               StudentExamDTO StuExam = new StudentExamDTO(SubjectID, StartTime, EndTime, Date, Room,Campus,Form);
+               boolean checkInsert = dao.insert(StuExam);
+               if(checkInsert){
+                   url = SUCCESS;
+                   request.setAttribute("ERROR", "Account "+ userID +" has been created succesfully!");
+                   
+               }else{
+                   userError.setError("Unknown error");
+                   request.setAttribute("USER_ERROR", userError);
+               }
+           } else{
+               request.setAttribute("USER_ERROR", userError);
+           }
+           
+           
+            
+            
+        } catch (Exception e) {
+            log("ERROR at Create Controller " + e.toString());
+        }finally{
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- ESMS-PROJECT-TEAM1/src/java/dao/UserDAO.java
+++ ESMS-PROJECT-TEAM1/src/java/dao/UserDAO.java
@@ -9,8 +9,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import utils.DBUtils;
+import java.text.SimpleDateFormat;
+
+
 
 /**
  *
@@ -18,6 +23,11 @@ import utils.DBUtils;
  */
 public class UserDAO {
     private static final String LOGIN = "SELECT Email, Account_ID, Password, Name, Gender, Address, DOB, RoleName FROM Account WHERE Email = ? AND Password = ?";
+    private static final String SEARCH  = "SELECT fullName, roleID, status, userID  from tblUsers WHERE fullName LIKE ? ORDER BY [userID] OFFSET 10*? ROWS FETCH NEXT 10 ROWS ONLY";
+    private static final String DELETE  = "DELETE FROM tblUsers WHERE userID=?";
+    private static final String UPDATE  = "UPDATE tblUsers SET fullName=?, roleID=? WHERE userID=?";
+    private static final String CHECK_DUPLICATE  = "SELECT roleID from tblUsers WHERE userID = ?";
+    private static final String INSERT = "INSERT INTO tblUsers(userID, fullName, roleID, password, status)" + "VALUES(?,?,?,?,1)";
     
     public UserDTO Login (String email, String password) throws ClassNotFoundException, SQLException{
         Connection conn = DBUtils.getConnection();
@@ -51,4 +61,143 @@ public class UserDAO {
     
         return user;     
     } 
+    
+    public static List<UserDTO> getListUser(String search, String pageNumber) throws SQLException{
+        List<UserDTO> listUser = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        try {
+          conn = DBUtils.getConnection();
+          if(conn != null){
+              ps = conn.prepareStatement(SEARCH);
+              ps.setString(1, "%"+search+"%");
+              ps.setString(2, ""+(Integer.parseInt(pageNumber)-1));
+              rs = ps.executeQuery();
+              while(rs.next()){
+                    String Email = rs.getString("Email");
+                    String AccountID = rs.getString("AccountID");
+                    String Name = rs.getString("Name");
+                    String Gender = rs.getString("Gender");
+                    String Address = rs.getString("Address");
+                    Date DOB = rs.getDate("DOB");
+                    String Roles = rs.getString("Roles");
+                    UserDTO newUser = new UserDTO(Email, AccountID, Name, Gender,Address,DOB,Roles);
+                    listUser.add(newUser);
+              }
+          }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(rs != null) rs.close();
+            if(ps != null) ps.close();
+            if(conn != null) conn.close();
+        }
+        return listUser;
+    }
+
+    public boolean delete(String userID) throws SQLException {
+        boolean isDeleted = false;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DBUtils.getConnection();
+            if(conn!=null){
+                ps = conn.prepareStatement(DELETE);
+                ps.setString(1, userID);
+                isDeleted = ps.executeUpdate()> 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(ps!=null) ps.close();
+            if(conn!=null); conn.close();
+        }
+        return isDeleted;
+    }
+
+    public boolean update(UserDTO user) throws SQLException {
+        boolean checkUpdate = false;
+        Connection conn = null;
+        PreparedStatement ps = null;
+        try {
+            conn = DBUtils.getConnection();
+            if(conn!=null){
+                ps = conn.prepareStatement(UPDATE);
+                ps.setString(1, user.getAccountID());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getGender());
+                ps.setString(4, user.getAddress());
+                ps.setDate(5, new java.sql.Date(user.getDOB().getTime()));
+                checkUpdate = ps.executeUpdate()> 0 ? true : false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(ps!=null) ps.close();
+            if(conn!=null); conn.close();
+        }
+        return checkUpdate;
+    }
+
+      public boolean checkDuplicate(String userID) throws SQLException {
+        boolean check = false;
+        UserDTO user = null;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(CHECK_DUPLICATE);
+                ptm.setString(1, userID);
+                rs = ptm.executeQuery();
+                if (rs.next()) {
+                    check = true;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return check;
+    }
+
+    public boolean insert(UserDTO user) throws SQLException, ClassNotFoundException{
+        boolean checkInsert = false;
+        Connection conn = null;
+        PreparedStatement ptm = null;
+        try {
+            conn = DBUtils.getConnection();
+            if (conn != null) {
+                ptm = conn.prepareStatement(INSERT);
+                ptm.setString(1, user.getAccountID());
+                ptm.setString(2, user.getName());
+                ptm.setString(3, user.getAddress());
+                ptm.setString(4, user.getGender());
+                ptm.setDate(5, new java.sql.Date(user.getDOB().getTime()));
+                checkInsert = ptm.executeUpdate() > 0 ? true : false;
+            }
+        } catch (Exception ex) {
+        } finally {
+            if (ptm != null) {
+                ptm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return checkInsert;
+    }
+   
 }
